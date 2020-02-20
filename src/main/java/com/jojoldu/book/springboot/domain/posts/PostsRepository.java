13f864c6ab72b36c36e26c14c6fa83176e1771e5
wldnhong <wldnhong64@cnspartner.com>
2020-02-20 22:01:27 +0900

    3일차

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // JpaRepository<Posts, Long> 상속받으면 기본 crud 기 자동으로 작성된다.
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
